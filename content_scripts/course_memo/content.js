d88a86c06ee66e180f447086db2eff1130b1e787
harunn-NotePC <haruharu41635@gmail.com>
2025-10-02 14:20:32 +0900

    メモ欄作成

--- content_scripts/course_memo/content.js
+++ content_scripts/course_memo/content.js
@@ -29,10 +29,10 @@ async function shouldRun() {
 // MutationObserverを使った要素出現監視関数をインポート
 const { observeElementAppearance } = require('../../util/mutationObserver.js');
 
-console.log('Course Memo content script loaded.');
 // ブラウザ読み込み時にメイン関数を実行。
 function main(){
-    addMemoToggle();
+    console.log(`機能(${FEATURE_KEY})を実行します。`);
+    observeElementAppearance('div#page', showFeatureSettingsPopup, document.body, true);
 }
 
 /** ポップアップを出すトグルを生成する関数
@@ -109,4 +109,60 @@ function addMemoToggle() {
         console.log('メモトグルを追加しました。');
     }, document.body, true);
 
+}
+
+/** メモ欄を表示する関数 */
+function showFeatureSettingsPopup() {
+    // 既にポップアップが存在する場合はなにもしない　(後で実装)
+
+    // メモ欄要素の作成
+    const sideMemoBar = document.createElement('div');
+    sideMemoBar.id = 'course-memo-sidebar';
+    sideMemoBar.style.width = '0';
+    sideMemoBar.style.height = '100%';
+    sideMemoBar.style.backgroundColor = '#f9f9f9';
+    sideMemoBar.style.padding = '0';
+    sideMemoBar.style.marginTop = '60px';
+    sideMemoBar.style.zIndex = '2';
+
+    // メモ欄とコース一覧を横並びにするためのコンテナ
+    const newWrapper = document.createElement('div');
+    newWrapper.style.display = 'flex';
+
+    // ラッピングするターゲット要素
+    const targetElement = document.querySelector('div#page');
+
+    // ターゲット要素の親要素を取得
+    const parentElement = targetElement.parentNode;
+
+    // 4. 新しい親要素を、元の要素の位置に挿入する
+    // (ここでは、元の親要素に対して、新しいラッパーを挿入します)
+    parentElement.insertBefore(newWrapper, targetElement);
+    
+    // ターゲット要素を新しいラッパーに移動
+    newWrapper.appendChild(targetElement);
+
+    // ターゲット要素を横いっぱいに広げる
+    targetElement.style.flexGrow = '1';
+
+    // メモ欄を新しいラッパーに追加
+    newWrapper.appendChild(sideMemoBar);
+}
+
+/** メモ欄を開らく関数 */
+function hideFeatureSettingsPopup() {
+    const sideMemoBar = document.getElementById('course-memo-sidebar');
+    if (sideMemoBar.style.width === '0px' || sideMemoBar.style.width === '0') {
+        sideMemoBar.style.width = '300px'; // メモ欄を開く
+        sideMemoBar.style.padding = '10px';
+    }
+}
+
+/** メモ欄を閉じる関数 */
+function closeFeatureSettingsPopup() {
+    const sideMemoBar = document.getElementById('course-memo-sidebar');
+    if (sideMemoBar.style.width !== '0px' || sideMemoBar.style.width !== '0') {
+        sideMemoBar.style.width = '0'; // メモ欄を閉じる
+        sideMemoBar.style.padding = '0';
+    }
 }
